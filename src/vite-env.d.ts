b2f168b65dcc8fc7df21bf6347cc4fcc3a04c8b0
gabraug <gabraug@icloud.com>
2025-11-07 23:47:51 -0300

    feat: vite-env.d.ts with tmdb base url and token

---
+++ src/vite-env.d.ts
@@ -0,0 +1,11 @@
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_TMDB_BASE_URL: string
+  readonly VITE_TMDB_TOKEN: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
+
