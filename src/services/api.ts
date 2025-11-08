7a76073cf4d98604d737b3a47c912559d65e1ca3
gabraug <gabraug@icloud.com>
2025-11-07 23:49:42 -0300

    fix: api.ts with meta.envs

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,7 @@
 import axios from 'axios'
 
-const API_BASE_URL = 'https://api.themoviedb.org/3'
-const API_TOKEN =
-  'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmNThhYzQwNDg2ZDRjNDI3NGY4YTMzMDlhZTZiYTIzOSIsIm5iZiI6MTc2MjU1NzQ3Ny41MTUsInN1YiI6IjY5MGU3ZTI1MTJjYTQ3NmQ1YWRkMTQ4MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Ohpzg5n2zU6Bysc95t8MerU72bmsfz1OlFMQq6bKyIY'
+const API_BASE_URL = import.meta.env.VITE_TMDB_BASE_URL
+const API_TOKEN = import.meta.env.VITE_TMDB_TOKEN
 
 const api = axios.create({
   baseURL: API_BASE_URL,
