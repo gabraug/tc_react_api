10e64769cb604cc6bfe75c429c991378abd504a6
gabraug <gabraug@icloud.com>
2025-11-07 23:08:26 -0300

    feat: api.ts with base_url and api_token open

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,15 +1,15 @@
 import axios from 'axios'
 
 const API_BASE_URL = 'https://api.themoviedb.org/3'
-const API_TOKEN = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmNThhYzQwNDg2ZDRjNDI3NGY4YTMzMDlhZTZiYTIzOSIsIm5iZiI6MTc2MjU1NzQ3Ny41MTUsInN1YiI6IjY5MGU3ZTI1MTJjYTQ3NmQ1YWRkMTQ4MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Ohpzg5n2zU6Bysc95t8MerU72bmsfz1OlFMQq6bKyIY'
+const API_TOKEN =
+  'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmNThhYzQwNDg2ZDRjNDI3NGY4YTMzMDlhZTZiYTIzOSIsIm5iZiI6MTc2MjU1NzQ3Ny41MTUsInN1YiI6IjY5MGU3ZTI1MTJjYTQ3NmQ1YWRkMTQ4MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Ohpzg5n2zU6Bysc95t8MerU72bmsfz1OlFMQq6bKyIY'
 
 const api = axios.create({
   baseURL: API_BASE_URL,
   headers: {
-    'Authorization': `Bearer ${API_TOKEN}`,
-    'accept': 'application/json',
+    Authorization: `Bearer ${API_TOKEN}`,
+    accept: 'application/json',
   },
 })
 
 export default api
-
