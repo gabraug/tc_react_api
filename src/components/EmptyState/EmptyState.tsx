dd6ebdf8fedf4b65800403688aa07365b7b21c8e
gabraug <gabraug@icloud.com>
2025-11-08 01:04:43 -0300

    fix: linters errors

--- src/components/EmptyState/EmptyState.tsx
+++ src/components/EmptyState/EmptyState.tsx
@@ -1,4 +1,4 @@
-import { memo, ReactNode } from 'react'
+import { memo, type ReactNode } from 'react'
 import Button from '../Button/Button'
 import Text from '../Text/Text'
 import { EmptyContainer } from './EmptyState.styles'
